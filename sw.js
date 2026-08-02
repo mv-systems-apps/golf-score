f620191f8c206985d6334a8e84a252a429437d4e
mv-systems-apps <m.j.vandervelden@gmail.com>
2026-08-02 22:35:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -11,7 +11,7 @@
 //   - er een bestand is toegevoegd/verwijderd uit APP_FILES hieronder, of
 //   - je een directe, volledige refresh wilt forceren i.p.v. de geleidelijke
 //     achtergrond-verversing.
-const CACHE_VERSION = 'golf-score-ef983d30481e';
+const CACHE_VERSION = 'golf-score-6c5ad362a3a6';
 
 // Bestanden die offline beschikbaar moeten zijn.
 const APP_FILES = [
