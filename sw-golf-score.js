214c89fc16f61f3b7690b708a0c2b6c5c98c0e55
mv-systems-apps <m.j.vandervelden@gmail.com>
2026-09-25 22:21:33 +0200

    Add files via upload

--- sw-golf-score.js
+++ sw-golf-score.js
@@ -13,7 +13,7 @@
 //   - er een bestand is toegevoegd/verwijderd uit APP_FILES hieronder, of
 //   - je een directe, volledige refresh wilt forceren i.p.v. de geleidelijke
 //     achtergrond-verversing.
-const CACHE_VERSION = 'golf-score-a1afd2a2b877';
+const CACHE_VERSION = 'golf-score-505ef5ee348e';
 
 // Bestanden die offline beschikbaar moeten zijn.
 const APP_FILES = [
